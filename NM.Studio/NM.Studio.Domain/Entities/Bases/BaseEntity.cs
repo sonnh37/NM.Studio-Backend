2b75e0b9ed76887c187f77510213c8929e3bebc4
Vuong Chan <tranminhvuong2k3@gmail.com>
2025-07-19 13:12:37 +0700

    Update

--- NM.Studio/NM.Studio.Domain/Entities/Bases/BaseEntity.cs
+++ NM.Studio/NM.Studio.Domain/Entities/Bases/BaseEntity.cs
@@ -6,11 +6,11 @@ public abstract class BaseEntity
 
     public string? CreatedBy { get; set; }
 
-    public DateTime CreatedDate { get; set; }
+    public DateTimeOffset? CreatedDate { get; set; }
 
     public string? LastUpdatedBy { get; set; }
 
-    public DateTime? LastUpdatedDate { get; set; }
+    public DateTimeOffset? LastUpdatedDate { get; set; }
 
     public bool IsDeleted { get; set; }
 }
